5381b7b91b261536a15cc55e45751476fe44a23c
sasi kiran <sasiking93@gmail.com>
2022-08-23 16:07:40 +0530

    added

---
+++ src/http_handler/index.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: 'http://localhost:8080/',
+    timeout: 1000,
+    headers: {'X-Custom-Header': 'foobar'}
+});
+
+export const getReq = (url, data) => {
+    let urlParams = null;
+    if (data.length) {
+        urlParams = '/';
+        for (const dataKey in data) {
+            urlParams += `&${dataKey}=${data[dataKey]}`;
+        }
+    }
+    return instance.get(url + urlParams)
+}
+
+export const postReq = (url, data) => {
+    return instance.post(url, data)
+
+}
+
+export const deleteReq = (url) => {
+    return instance.delete(url)
+
+}
+
+export const patchReq = (url, data) => {
+    return instance.patch(url, data)
+}
